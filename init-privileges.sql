50c465e5c1cd9dfda09dc4408ba3284d812a0409
HussainRafiq <71262197+HussainRafiq@users.noreply.github.com>
2026-02-06 22:12:17 +0500

    fix

--- init-privileges.sql
+++ init-privileges.sql
@@ -3,7 +3,7 @@
 
 -- Full access to all current & future prod_* databases
 GRANT ALL PRIVILEGES ON `prod\_%`.* TO 'plumoai_user'@'%';
-GRANT ALL PRIVILEGES ON `plumoai_prod_model` TO 'plumoai_user'@'%';
+GRANT ALL PRIVILEGES ON `plumoai_prod\_%`.* TO 'plumoai_user'@'%';
 
 -- Allow runtime database creation (API / user-click DB creation)
 GRANT CREATE ON *.* TO 'plumoai_user'@'%';
