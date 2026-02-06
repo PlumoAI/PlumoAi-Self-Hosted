39b1e13c2aa3731ba49cbc7a9dc0689588b91370
HussainRafiq <71262197+HussainRafiq@users.noreply.github.com>
2026-02-06 22:16:23 +0500

    fix

--- init-privileges.sql
+++ init-privileges.sql
@@ -1,9 +1,9 @@
 -- User is created by MySQL from secrets (mysql_user, mysql_password).
 -- This runs only on first container startup (fresh mysql_data volume).
 
--- Full access to all current & future prod_* databases
-GRANT ALL PRIVILEGES ON `prod\_%`.* TO 'plumoai_user'@'%';
-GRANT ALL PRIVILEGES ON `plumoai_prod\_%`.* TO 'plumoai_user'@'%';
+-- Full access (including DROP, GRANT) on prod_* and plumoai_* databases
+GRANT ALL PRIVILEGES ON `prod\_%`.* TO 'plumoai_user'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `plumoai\_%`.* TO 'plumoai_user'@'%' WITH GRANT OPTION;
 
 -- Allow runtime database creation (API / user-click DB creation)
 GRANT CREATE ON *.* TO 'plumoai_user'@'%';
